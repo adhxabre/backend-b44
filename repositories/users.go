67dc3b27eff338cbc77cc4f7e9136851df7f4fbd
suryaa6666 <suryaelidanto@gmail.com>
2023-02-16 13:45:42 +0700

    template

--- repositories/users.go
+++ repositories/users.go
@@ -25,14 +25,12 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
-	err := r.db.Find(&users).Error
 
 	return users, err
 }
 
 func (r *repository) GetUser(ID int) (models.User, error) {
 	var user models.User
-	err := r.db.First(&user, ID).Error
 
 	return user, err
 }
