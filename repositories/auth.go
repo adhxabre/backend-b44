a91a67695c662bed210b02e2549297c5d49ab82a
suryaa6666 <suryaelidanto@gmail.com>
2023-02-16 14:42:48 +0700

    template

--- repositories/auth.go
+++ repositories/auth.go
@@ -13,9 +13,3 @@ type AuthRepository interface {
 func RepositoryAuth(db *gorm.DB) *repository {
 	return &repository{db}
 }
-
-func (r *repository) Register(user models.User) (models.User, error) {
-	err := r.db.Create(&user).Error
-
-	return user, err
-}
--- routes/auth.go
+++ routes/auth.go
@@ -1,16 +1,9 @@
 package routes
 
 import (
-	"dumbmerch/handlers"
-	"dumbmerch/pkg/mysql"
-	"dumbmerch/repositories"
-
 	"github.com/labstack/echo/v4"
 )
 
 func AuthRoutes(e *echo.Group) {
-	authRepository := repositories.RepositoryAuth(mysql.DB)
-	h := handlers.HandlerAuth(authRepository)
 
-	e.POST("/register", h.Register)
 }
